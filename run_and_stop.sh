399631ab9f2625b58e678efea9145257631641d5
qays3 <167983721+qays3@users.noreply.github.com>
2025-05-25 18:47:18 +0300

    update the progress

--- run_and_stop.sh
+++ run_and_stop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <docker_run_command>"
     exit 1
 fi
 
@@ -12,23 +11,19 @@ log_message() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> "$log_file"
 }
 
-> "$log_file"
-
 image_name=$(echo "$docker_run_command" | awk '{print $NF}')
 container_name="container_$(date +%s%N | sha256sum | head -c10)"
 docker_run_command_with_name=$(echo "$docker_run_command" | sed "s/docker run/docker run --name $container_name/")
 
-if ! eval "$docker_run_command_with_name >/dev/null 2>&1 &"; then
-    log_message "Failed to execute Docker run command: $docker_run_command_with_name"
+log_message "Running: $docker_run_command_with_name"
+eval "$docker_run_command_with_name" >> "$log_file" 2>&1
+if [ $? -ne 0 ]; then
+    log_message "Failed to execute Docker run command."
     exit 1
 fi
 
 log_message "Docker container started with name: $container_name"
-
-
 sleep 3600
-
-
-docker stop $container_name >/dev/null 2>&1
-docker rm $container_name >/dev/null 2>&1
+docker stop $container_name >> "$log_file" 2>&1
+docker rm $container_name >> "$log_file" 2>&1
 log_message "Docker container $container_name has been stopped and removed."
